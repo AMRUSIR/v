d020af69103701ce9d366eedd3069f147db639ba
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-05-19 09:40:48 +0530

    // Copyright (C) 2020 Afx-Abu //

--- plugins/insta.js
+++ plugins/insta.js
@@ -1,8 +1,4 @@
-/* Code Credits: souravkl11, raganork-api
-Please give credit to the creator :)
-(c) souravkl11 2022 All rights reserved
-*/
-const skl = require('../events');
+const jsl = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const got = require("got");
@@ -17,8 +13,7 @@ var downloading = "_Downloading_";
 var need_acc = "*_Need an instagram username!_*";
 var fail = "*_Download failed! Check your link and try again_*";
 var need_acc_s = "_Need an instagram username or link!_";
-let sourav = setting.WORKTYPE == 'public' ? false : true
-skl.addCommand({ pattern: 'insta ?(.*)', fromMe: sourav, desc:'Downloads post/reel/igtv from instagram',usage:'insta link or reply to a link'}, (async (msg, query) => {
+jsl.addCommand({ pattern: 'insta ?(.*)', fromMe: true, desc:'Downloads post/reel/igtv from instagram',usage:'insta link or reply to a link'}, (async (msg, query) => {
 var q = !msg.reply_message.message ? query[1] : msg.reply_message.message
 if (!q)  return await msg.client.sendMessage(msg.jid, '_Unable to read link from message!_', MessageType.text, {quoted: msg.data});
 if (q && !q.includes('instagram.com')) return await msg.client.sendMessage(msg.jid, need, MessageType.text, {quoted: msg.data});
@@ -48,14 +43,14 @@ if (res.links[0].url.includes('jpg')) return await msg.client.sendMessage(msg.ji
     
 }
 }));
-skl.addCommand({ pattern: 'ig ?(.*)', fromMe: sourav, desc:'Gets account info from instagram',usage:'ig username'}, (async (msg, query) => {
+jsl.addCommand({ pattern: 'ig ?(.*)', fromMe: true, desc:'Gets account info from instagram',usage:'ig username'}, (async (msg, query) => {
     if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc, MessageType.text, {quoted: msg.data});
     var res = await getStalk(query[1])
     if (res === "false") return await msg.client.sendMessage(msg.jid, "_Username invalid!_", MessageType.text, {quoted: msg.data})
     var buffer = await skbuffer(res.hd_profile_pic_url_info.url)
     await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_*Name:*_ ' + `${res.fullname}` + '\n _*Bio:*_ ' + `${res.biography}`+ '\n _*Private account:*_ ' + `${res.is_private} ` + '\n _*Followers:*_ ' + `${res.followers}` + '\n _*Following:*_ ' + `${res.following}` + '\n _*Posts:*_ ' + `${res.post_count}` + '\n _*Verified:*_ ' + `${res.is_verified} ` + '\n _*IGTV videos:*_ ' + `${res.total_igtv_videos}`, quoted: msg.data});
     }));
-skl.addCommand({ pattern: 'story ?(.*)', fromMe: sourav, desc:'Downloads full/single story from instagram',usage:'.story username or link'}, (async (msg, query) => {
+jsl.addCommand({ pattern: 'story ?(.*)', fromMe: true, desc:'Downloads full/single story from instagram',usage:'.story username or link'}, (async (msg, query) => {
 if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc_s, MessageType.text, {quoted: msg.data});
 var user = query[1];
 var res = await getStory(user,v)
