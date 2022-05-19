851aca376620025a6d6487fb29de6f0a8d33a26e
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-05-19 10:02:09 +0530

    Fix

--- plugins/fancy_text.js
+++ plugins/fancy_text.js
@@ -1,6 +1,5 @@
 const {addCommand} = require('../events')
 const {Fancy} = require('raganork-bot')
-const {WORKTYPE} = require('../config');
 addCommand({pattern: 'fancy ?(.*)', fromMe: true, desc: 'Creates fancy text fonts'}, (async (message, match) => {
 if (!match[1]||!message.reply_message.message) return await message.sendReply("Reply to any message with .fancy number\n"+Fancy("example","32"))
 try { var result = Fancy(message.reply_message.message,match[1]) } catch (e) {return await message.sendReply(e.message)}
