8e35bd6b0095d24dde5bca2d6c4a0e5861110ce3
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-05-19 09:58:50 +0530

    Fic

--- plugins/doc.js
+++ plugins/doc.js
@@ -5,8 +5,7 @@ let e = require('../events');
 let c = require('../config');
 let i = require('raganork-bot');
 const { fromBuffer } = require('file-type')
-let fm = c.WORKTYPE == 'public' ? false : true
-e.addCommand({ pattern: 'doc (.*)', fromMe: fm, desc: 'Converts media messages to documents with given filenames' },(async (m, text) => {
+e.addCommand({ pattern: 'doc (.*)', fromMe: true, desc: 'Converts media messages to documents with given filenames' },(async (m, text) => {
 let fnm = !text[1] ? 'File' : text[1]
 let fn = await m.client.downloadMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage})
 let mime = await fromBuffer(fn)
