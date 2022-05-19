cbcd98aea6f39477acf5e4711cebb3526fda7f44
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-05-19 07:43:27 +0530

    Fix👀

--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -3,8 +3,7 @@ const New = require('../events');
 const s = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
 const v = s.SESSION
-const sourav = s.WORKTYPE == 'public' ? false : true
-New.addCommand({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',fromMe: sourav}, async (msg, query) => {
+New.addCommand({pattern: 'true ?(.*)', desc: 'Searches for number in truecaller!',fromMe: true}, async (msg, query) => {
 if (!query[1] && !msg.reply_message) return await msg.reply("_Give me any number or reply to any user!_");
 if (query[1].includes('/')) return await msg.client.sendMessage(msg.jid, 'Wrong format! \n\n .true +91 6282344739', MessageType.text, {quoted: msg.data})
 var go;
